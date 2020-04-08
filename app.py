b645d1aaa08ab35530bfdc6aa411d0165b8b3eba
dombeesley <quigley_beesley@hotmail.com>
2020-04-08 09:01:34 +0000

    initial commit

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template, redirect, request, url_for
+
+
+
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP', '0.0.0.0'),
+            port=int(os.environ.get('PORT', '5000')),
+            debug=True)
