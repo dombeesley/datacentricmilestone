e937d04104a6355794693ce0a87c6a4345f0f32f
dombeesley <quigley_beesley@hotmail.com>
2020-04-13 10:42:30 +0000

    add login and register pages

--- app.py
+++ app.py
@@ -41,8 +41,17 @@ def view_book(book_id):
     return render_template('viewbook.html', book=the_book)
 
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP', '0.0.0.0'),
             port=int(os.environ.get('PORT', '5000')),
             debug=True)
-            
