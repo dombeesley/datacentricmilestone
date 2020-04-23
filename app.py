b13fe72bb7fc283397004c77eed26fa82f2fc55a
dombeesley <quigley_beesley@hotmail.com>
2020-04-23 13:08:03 +0000

    add env.py file

--- app.py
+++ app.py
@@ -2,12 +2,15 @@ import os
 from flask import Flask, render_template, redirect, request, url_for, session
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from os import path
+if path.exists("env.py"):
+  import env 
 
 app = Flask(__name__)
 
 
 app.config["MONGO_DBNAME"] = 'data_centric_books'
-app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@myfirstcluster-mtbzq.mongodb.net/data_centric_books?retryWrites=true&w=majority'
+app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
 
 
 mongo = PyMongo(app)
@@ -17,10 +20,12 @@ mongo = PyMongo(app)
 def home():
     return render_template('home.html')
 
+
 # Page to add a book
 @app.route('/addbook')
 def addbook():
     return render_template('addbook.html')
+
 
 # Adding a book to the database
 @app.route('/insert_book', methods=['POST'])
@@ -29,10 +34,12 @@ def insert_book():
     library.insert_one(request.form.to_dict())
     return redirect(url_for('see_library'))
 
+
 # View all books in the database
 @app.route('/library')
 def see_library():
     return render_template('library.html', library=mongo.db.library.find())
+
 
 # Viewing a book's information
 @app.route('/view_book/<book_id>')
@@ -40,11 +47,13 @@ def view_book(book_id):
     the_book = mongo.db.library.find_one({"_id": ObjectId(book_id)})
     return render_template('viewbook.html', book=the_book)
 
+
 # Page for user to edit review
 @app.route('/editreview/<book_id>')
 def edit_review(book_id):
     the_book = mongo.db.library.find_one({"_id": ObjectId(book_id)})
     return render_template('editreview.html', book=the_book)
+
 
 # Updating review in database
 @app.route('/update_review/<book_id>', methods=["POST"])
@@ -62,11 +71,13 @@ def update_review(book_id):
     })
     return redirect(url_for('see_library'))
 
+
 # Deleting book's entry from database
 @app.route('/delete_review/<book_id>')
 def delete_review(book_id):
     mongo.db.library.remove({'_id': ObjectId(book_id)})
     return redirect(url_for('see_library'))
+
 
 # Page to view books in contemporary genre
 @app.route('/contemp')
@@ -115,6 +126,7 @@ def scifi():
 def thriller():
     return render_template('genre/thriller.html', library=mongo.db.library.find())
 
+
 # Logging in
 @app.route('/loginpage')
 def loginpage():
@@ -133,6 +145,7 @@ def login():
 
     return render_template('tryagain.html')
 
+
 # Registering for the site
 @app.route('/register', methods=['POST', 'GET'])
 def register():
@@ -143,11 +156,12 @@ def register():
         if existing_user is None:
             users.insert({'username': request.form['username'], 'password': request.form['password']})
             session['username'] = request.form['username']
-            return redirect(url_for('home'))
+            return redirect(url_for('see_library'))
 
         return render_template('sorry.html')
 
     return render_template('register.html')
+
 
 # Logging out
 @app.route('/logout')
